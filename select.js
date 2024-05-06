620662f6aba81552cca0a67752220434ba0331e4
My-Designer <cb.theapp@gmail.com>
2024-05-06 21:21:53 +0200

    Imitiate select tag more accurately

--- select.js
+++ select.js
@@ -11,94 +11,78 @@ function appDiv(element, className) {
 
 function dobby(id) {return document.getElementById(id);}
 
-/*
- * Helper functions
- */
+function scrollInto(e) {e.scrollIntoView({behavior: 'smooth', block: 'nearest'});}
 
+function logevent(e) {console.log('Event: '+e)}
 
-function scrollInto(e) {
-  e.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-}
-
-/*
- * Select Component
- * Accepts a combobox element and an array of string options
- */
 class Select {
 
-moveClass(ii, i1, name) {
+moveClass(i1, name) {
   const children = this.elTable.children;
-  console.log(`moving children ${!!children} ii=${ii} i1=${i1} name=`+name);
-  const i0 = this.i[ii], c0 = children[i0], c1 = children[i1];
-  console.log(`i0=${i0} i1=${i1} c0=${!!c0} c1=${!!c1}`);
-  if (!c0) {
-    debugger;
-  }
-  children[this.i[ii]].classList.remove(name);
-  children[this.i[ii]=i1].classList.add(name);
+  const i0 = this[name], c0 = children[i0], c1 = children[i1];
+  console.log(`moving class ${name} ${i0} => ${i1}`);
+  children[this[name]].classList.remove(name);
+  children[this[name]=i1].classList.add(name);
 }
 
-updateMenuState(open) {
+setMarked(index) {
+  if (!this.open) return;
+  this.moveClass(index, 'marked');
+  scrollInto(this.elTable.children[index]);
+}
+
+setChosen(index) {
+  this.moveClass(index, 'chosen');
+  this.elCombo.innerText = this.options[index];
+  this.setMarked(index);
+}
+
+chooseMarked() {
+  this.setChosen(this.marked);
+};
+
+updateMenuState(open, choose) {
   if (this.open == open);
   else if (this.open = open) {
     this.el.classList.add('open');
-    const index = this.i[0];
-    console.log('updateMenuState '+index);
-    scrollInto(this.elTable.children[index]);
+    this.setMarked(this.chosen);
+    this.elTable.focus();
   } else {
-    const index = this.i[1];
-    this.moveClass(0, index, 'option-current');
     this.el.classList.remove('open');
+    if (choose) this.chooseMarked();
     scrollInto(this.elCombo);
-    this.elCombo.focus(); // move focus back to the combobox, if needed
+    this.elCombo.focus();
   }
-};
-
-selectOption() {
-  const i = this.i[0];
-  this.elCombo.innerText = this.options[i]; // update displayed value
-  this.moveClass(1, i, 'aria-selected');
-};
-
-onOptionChange(index) {
-  this.moveClass(0, index, 'option-current');
-  if(this.open) scrollInto(this.elTable.children[index]);
-  else this.selectOption();
-  
-};
-
-onOptionClick(index) {
-  console.log('onOptionClick>() '+index);
-  this.updateMenuState(false);
-  this.onOptionChange(index);
-};
-
-onOptionMouseDown() {
-  // Clicking an option will cause a blur event,
-  // but we don't want to perform the default keyboard blur action
-  this.ignoreBlur = true;
 };
 
 onComboBlur(event) {
-  // do nothing if relatedTarget is contained within elTable
-  if (this.elTable.contains(event.relatedTarget)) return;
+  logevent('blur head');
+  if (this.elTable == event.relatedTarget) return;
+  this.updateMenuState(false, true);
+};
 
-  // select current option and close
-  if (this.open) {
-    this.selectOption();
-    this.updateMenuState(false);
-  }
+onTableBlur(event) {
+  logevent('blur table');
+  if (this.elCombo == event.relatedTarget) return;
+  this.updateMenuState(false);
 };
 
 onComboClick() {
+  logevent('comboclick');
   this.updateMenuState(!this.open);
 };
 
+onTableClick(event) {
+  logevent('tableclick');
+  this.updateMenuState(false, true)
+  event.stopPropagation();
+};
+
 onKeyDown(event) {
+  logevent('keydown');
   if(event.isComposing || event.keyCode == 229) return;
   if(event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) return;
-  const key = event.key== ' ' ? 'Enter' : event.key;
-  const max = this.options.length - 1;
+  const key = event.key, max = this.options.length - 1;
 
   const move =
     key == 'Home'       ? -max :
@@ -108,56 +92,47 @@ onKeyDown(event) {
     key == 'ArrowUp'    ?   -1 :
     key == 'ArrowDown'  ?    1 :
   0;
-  if (move) this.onOptionChange(Math.max(0,Math.min(this.i[0]+move,max)));
-  else {
-    let nopen = !this.open;
-    if(key == 'Escape') nopen = false;
-    else if(key != 'Enter') return;  // pass the event
-    else if(!nopen) this.selectOption();
-    this.updateMenuState(nopen);
-  }
+  if (move) {
+    const index = this.open ? this.marked : this.chosen;
+    this.setChosen(Math.max(0,Math.min(index+move,max)));
+  } else if(key == 'Escape') this.updateMenuState();
+  else if(key == 'Enter') this.updateMenuState(!this.open, true);
+  else return; // pass the event
 
   event.preventDefault();
 };
 
-constructor (options = []) {
+onMouseEnter(event) {
+  const index = event.target.rowIndex;
+  logevent('mouse enter '+index);
+  this.setMarked(index);
+};
 
-  // data
+constructor (options = []) {
   this.options = options;
 
-  // state
-  this.open = false;
-  this.i = [0,0]; // index of active candidate and selected element
+  this.chosen = this.marked = 0;
 
-  // element refs
   const el = this.el = document.createElement('div');
   el.className = "combo";
   (this.elCombo = appDiv(el, 'combo-input')).tabIndex = 0;
   (this.elTable = appType(el, 'table', 'combo-menu')).tabIndex = -1;
 
-  // select first option by default
-  this.elCombo.innerText = this.options[0];
+  const listen = (element, event, fun) => element.addEventListener(event, fun.bind(this));
 
-  // add event listeners
-  this.elTable.addEventListener('focusout', this.onComboBlur.bind(this));
-  this.elCombo.addEventListener('blur', this.onComboBlur.bind(this));
-  this.elCombo.addEventListener('click', this.onComboClick.bind(this));
-  this.el     .addEventListener('keydown', this.onKeyDown.bind(this));
+  listen(this.elTable, 'blur', this.onTableBlur);
+  listen(this.elCombo, 'click', this.onComboClick);
+  listen(this.elTable, 'click', this.onTableClick);
+  listen(this.elCombo, 'keydown', this.onKeyDown);
+  listen(this.elTable, 'keydown', this.onKeyDown);
 
-  // create options
-  const onClick = event => {
-    this.onOptionClick(event.target.parentElement.rowIndex);
-    event.stopPropagation();
-  };
-  // Clicking an option will cause a blur event,
-  // but we don't want to perform the default keyboard blur action
-  const onDown = event => this.ignoreBlur = true;
   for (const option of options) {
     const row = appType(this.elTable, 'tr', 'combo-option');
     appType(row, 'td').innerText = option;
-    row.addEventListener('click', onClick);
-    row.addEventListener('mousedown', onDown);
+    listen(row, 'mouseenter', this.onMouseEnter);
   };
+
+  this.chooseMarked();
 };
 
 }
